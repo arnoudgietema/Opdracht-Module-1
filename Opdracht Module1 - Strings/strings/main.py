4d60ee3cb3eaaa3837ae5715356a6a0ff0034897
arnoudgietema <116758777+arnoudgietema@users.noreply.github.com>
2023-05-09 20:43:35 +0200

    Nieuwe files geupload na aanpassingen odracht

--- Opdracht Module1 - Strings/strings/main.py
+++ Opdracht Module1 - Strings/strings/main.py
@@ -12,17 +12,17 @@ goal_1 = 54
 strgoal1 = str(goal_1)
 
 #state which players scored the goals during the EURO1988 Final and when
-Score_stat1 = scorer1 + 'scored in the ' + strgoal0 + 'nd minute'
+Score_stat1 = scorer1 + 'scored in the ' + strgoal0 + 'nd minute\n'
 Score_stat2 = scorer2 + 'scored in the ' + strgoal1 + 'th minute'
-report=Score_stat1+' '+Score_stat2
+report=Score_stat1+Score_stat2
 scorers = scorer1+strgoal0+', '+scorer2+strgoal1
-scorers_explained = 'The players who scored the goals in the EURO1988 final were: \n' + Score_stat1 + ' and\n' + Score_stat2
+scorers_explained = Score_stat1 + ' ' + Score_stat2
 print(scorers_explained)
 
 # Save the Scorers to a CSV report
 import os
 
-file_path = r'C:\Users\Arnoud\Documents\Werk\Magniv\Cursus Data Analytics with Python\Winc\strings\RGMvB.csv'
+file_path = r'C:\Users\Arnoud\Documents\GitHub\Opdracht-Module-1\Opdracht Module1 - Strings\strings\RGMvB.csv'
 if os.path.exists(file_path):
     print('The score statistics of the EURO1988 final have already been saved in a csv file')
 else:
@@ -33,16 +33,18 @@ else:
 
 
 player = 'Hans_van_Breukelen'
-first_name = player[0:4]
-last_name = player[5:8]+' '+player[9:18]
+first_name = player[0:17]
+last_name = player[0:18]
 last_name_len = len(last_name)
-name_short = player[0:1] + '. ' + player[5:8] + ' ' + player[9:18]
-print(name_short)
-print(first_name)
-print(last_name)
-print(first_name+'! '+first_name+'! '+first_name+'!'+' Wat een geweldige save!!')
-print(2 !=3)
-file_path = r'C:\Users\Arnoud\Documents\Werk\Magniv\Cursus Data Analytics with Python\Winc\strings\HvB.csv'
+name_short = player[0:1]+'.'+player[17:18]
+
+chant = (first_name+'! ')*16+first_name+'!'
+good_chant = True
+print(chant)
+print (2!=3)
+
+
+file_path = r'C:\Users\Arnoud\Documents\GitHub\Opdracht-Module-1\Opdracht Module1 - Strings\strings\HvB.csv'
 if os.path.exists(file_path):
     print('The goalkeeper of the EURO1988 final has already been saved in a csv file')
 else:
