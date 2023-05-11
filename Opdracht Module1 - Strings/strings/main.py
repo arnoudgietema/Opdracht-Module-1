97cc3a4f798c66ae662e28b59e2a249990e5d17d
arnoudgietema <116758777+arnoudgietema@users.noreply.github.com>
2023-05-11 11:18:26 +0200

    Add files via upload
    In de repository de aangepaste opdracht obv de feedback. NB. de code mbt. de file opslag heb ik eruit gelaten en apart bewaard, want ik begrijp uit je feedback dat dat geen onderdeel was van de opdracht. Dus dat zit er nu niet meer bij

--- Opdracht Module1 - Strings/strings/main.py
+++ Opdracht Module1 - Strings/strings/main.py
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 # Do not modify these lines
 __winc_id__ = '71dd124b4a6e4d268f5973db521394ee'
 __human_name__ = 'strings'
@@ -66,4 +67,65 @@ else:
 
 
 
+=======
+# Do not modify these lines
+__winc_id__ = '71dd124b4a6e4d268f5973db521394ee'
+__human_name__ = 'strings'
+
+# Add your code after this line 
+#definition of variables
+scorer1 = 'Ruud Gullit'
+scorer2 = 'Marco van Basten'
+goal_0 = 32
+strgoal0 = str(goal_0)
+goal_1 = 54
+strgoal1 = str(goal_1)
+
+
+#state which players scored the goals during the EURO1988 Final and when
+#scorers1 = scorer1 + ' ' + strgoal0 + ',\n' + scorer2 + ' ' + strgoal1
+#print(scorers1)
+
+scorers = f'{scorer1} {strgoal0}, {scorer2} {strgoal1}'
+print(scorers)
+report = f'{scorer1} scored in the {goal_0}nd minute\n{scorer2} scored in the {goal_1}th minute'
+print(report)
+
+# Name a player, first name, last name, initial and last name, chant
+
+player = 'Marco van Basten'
+first_name = player[0:player.find(' ')]
+last_name = player[player.find(' ')+1:(len(player))]
+last_name_len = len(last_name)
+initial = first_name[0:1]
+name_short = initial+'.'+' '+last_name
+chant =  (first_name+'! ')*(len(first_name)-1)+first_name+'!'
+good_chant = True
+
+#print output
+print(first_name)
+print(last_name)
+print(name_short)
+print(last_name_len)
+print(chant)
+print(good_chant)
+print(2!=3)
+
+
+
+
+
+
+
+   
+      
+       
+        
+
+
+
+
+
+
+>>>>>>> Stashed changes
  
